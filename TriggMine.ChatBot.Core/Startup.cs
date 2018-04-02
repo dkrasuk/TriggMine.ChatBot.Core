a8964e68d9674e15a047f55c9e305c23719ede3d
Dmitrij Krasiuk <dmitrij.krasyuk@gmail.com>
2018-04-02 23:34:32 +0300

    ADD VIEW

--- TriggMine.ChatBot.Core/Startup.cs
+++ TriggMine.ChatBot.Core/Startup.cs
@@ -73,7 +73,13 @@ namespace TriggMine.ChatBot.Core
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             telegramBotService.GetBot();
             //Connect Swagger
