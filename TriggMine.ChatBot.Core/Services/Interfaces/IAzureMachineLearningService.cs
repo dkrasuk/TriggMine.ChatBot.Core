92c3cf4d7e0c5ceb5d2faad5cd0a0e087138038c
Dmitrij Krasiuk <dmitrij.krasyuk@gmail.com>
2018-07-27 23:27:21 +0300

    add ML Azure logic

---
+++ TriggMine.ChatBot.Core/Services/Interfaces/IAzureMachineLearningService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TriggMine.ChatBot.Core.Services.Interfaces
+{
+    public interface IAzureMachineLearningService
+    {
+        Task<string> AnalyzePhotoMLAzureAsync(string imagePath);
+    }
+}
